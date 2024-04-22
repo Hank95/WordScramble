fab0e44ed4655b719b4dba355eb3c73716a6b9ca
Henry Pendleton <hhpendleton@gmail.com>
2024-04-22 10:39:37 -0400

    Initial Commit

---
+++ WordScramble/WordScrambleApp.swift
@@ -0,0 +1,17 @@
+//
+//  WordScrambleApp.swift
+//  WordScramble
+//
+//  Created by Henry Pendleton on 4/22/24.
+//
+
+import SwiftUI
+
+@main
+struct WordScrambleApp: App {
+    var body: some Scene {
+        WindowGroup {
+            ContentView()
+        }
+    }
+}
