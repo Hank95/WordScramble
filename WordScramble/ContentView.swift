e0cf2fe6d341405263efebbf318f9548cfc13bba
Henry Pendleton <hhpendleton@gmail.com>
2024-04-24 14:23:12 -0400

    added challenge funtionalitly

--- WordScramble/ContentView.swift
+++ WordScramble/ContentView.swift
@@ -11,6 +11,11 @@ struct ContentView: View {
     @State private var usedWords = [String]()
     @State private var rootWord = ""
     @State private var newWord = ""
+    @State private var score = 0
+    
+    @State private var errorTitle = ""
+    @State private var errorMessage = ""
+    @State private var showingError = false
 
     var body: some View {
         NavigationStack {
@@ -19,6 +24,17 @@ struct ContentView: View {
                     TextField("Enter your word", text: $newWord)
                         .textInputAutocapitalization(.never)
                 }
+                Section(header: Text("Total Score")) {
+                    VStack {
+                        Spacer()
+                        Text("\(score)")
+                            .frame(maxWidth: .infinity, alignment: .center)
+                            .font(.system(size: 24, weight: .bold))
+                        Spacer()
+                    }
+                }
+        
+                
                 Section{
                     ForEach(usedWords, id: \.self){ word in
                         HStack{
@@ -29,12 +45,18 @@ struct ContentView: View {
                 }
             }
             .navigationTitle(rootWord)
+            .toolbar{
+                Button("Restart", action: startGame)
+            }
             .onSubmit {
                 addNewWord()
             }
             .onAppear(perform: {
                 startGame()
             })
+            .alert(errorTitle, isPresented: $showingError){ } message: {
+                Text(errorMessage)
+            }
         }
     }
     
@@ -42,13 +64,33 @@ struct ContentView: View {
         let answer = newWord.lowercased().trimmingCharacters(in: .whitespacesAndNewlines)
         
         guard answer.count > 0 else {return}
+        guard answer.count > 3 else {
+            wordError(title: "Word too short", message: "Try again with a longer word")
+            return
+        }
+        
+        guard isOriginal(word: answer) else {
+            wordError(title: "Word already used", message: "Be more orginal")
+            return
+        }
+        guard isPossible(word: answer) else {
+            wordError(title: "Word not possible", message: "You can't spell that word from '\(rootWord)'")
+            return
+        }
+        
+        guard isReal(word: answer) else {
+            wordError(title: "Word not recognized", message: "You can't just make them up")
+            return
+        }
         withAnimation{
             usedWords.insert(answer, at: 0)
         }
+        score = score + answer.count
         newWord = ""
     }
     
     func startGame() {
+        score = 0
         if let startWordsURL = Bundle.main.url(forResource: "start", withExtension: "txt"){
             if let startWords = try? String(contentsOf: startWordsURL) {
                 let allWords = startWords.components(separatedBy: "\n")
@@ -57,6 +99,35 @@ struct ContentView: View {
             }
         }
         fatalError("Could not load start.txt from bundle")
+    }
+    
+    func isOriginal(word: String) -> Bool {
+        !usedWords.contains(word)
+    }
+    func isPossible(word: String) ->Bool{
+        var tempWord = rootWord
+        for letter in word {
+            if let pos = tempWord.firstIndex(of: letter) {
+                tempWord.remove(at: pos)
+            } else {
+                return false
+            }
+        }
+        return true
+    }
+    
+    func isReal(word: String) -> Bool {
+        let checker = UITextChecker()
+        let range = NSRange(location: 0, length: word.utf16.count)
+        let misspelledRange = checker.rangeOfMisspelledWord(in: word, range: range, startingAt: 0, wrap: false, language: "en")
+
+        return misspelledRange.location == NSNotFound
+    }
+    
+    func wordError(title: String, message: String) {
+        errorMessage = message
+        errorTitle = title
+        showingError = true
     }
 }
 
